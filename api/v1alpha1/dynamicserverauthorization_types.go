9c94b9f384c017c95cdb16a8fa259fff249e8692
Ripta Pasay <ripta@users.noreply.github.com>
2022-11-20 21:12:45 -0800

    *: scaffold a new DynamicServerAuthorization kind

---
+++ api/v1alpha1/dynamicserverauthorization_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022 Ripta Pasay.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DynamicServerAuthorizationSpec defines the desired state of DynamicServerAuthorization
+type DynamicServerAuthorizationSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of DynamicServerAuthorization. Edit dynamicserverauthorization_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// DynamicServerAuthorizationStatus defines the observed state of DynamicServerAuthorization
+type DynamicServerAuthorizationStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// DynamicServerAuthorization is the Schema for the dynamicserverauthorizations API
+type DynamicServerAuthorization struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DynamicServerAuthorizationSpec   `json:"spec,omitempty"`
+	Status DynamicServerAuthorizationStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DynamicServerAuthorizationList contains a list of DynamicServerAuthorization
+type DynamicServerAuthorizationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DynamicServerAuthorization `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DynamicServerAuthorization{}, &DynamicServerAuthorizationList{})
+}
